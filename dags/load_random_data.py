f1e8bd9e451039be79d3b49192d22352f2ffb4e9
Dan <danielmcollins1996@gmail.com>
2021-12-03 14:45:03 -0700

    using postgres metadata, also grouping dags

--- dags/load_random_data.py
+++ dags/load_random_data.py
@@ -17,47 +17,50 @@ default_args = {
     'start_date': days_ago(2),
 }
 
-ingestion_dag = DAG(
-        'load_poopoo',
+with DAG(
+        dag_id='load_poopoo',
         default_args=default_args,
-)
+) as ingestion_dag:
 
 
-with TaskGroup('poopoo stuff') as poopoo_group:
-    task_1 = PythonOperator(
-        task_id='transform_poopoo',
-        python_callable=transform_poopoo,
-        dag=ingestion_dag,
-    )
+    with TaskGroup('poopoo_stuff') as poopoo_group:
+        task_1 = PythonOperator(
+            task_id='transform_poopoo',
+            python_callable=transform_poopoo,
+            dag=ingestion_dag,
+        )
 
-    task_2 = PythonOperator(
-        task_id='load_poopoo',
-        python_callable=load_poopoo,
-        dag=ingestion_dag,
-    )
+        task_2 = PythonOperator(
+            task_id='load_poopoo',
+            python_callable=load_poopoo,
+            dag=ingestion_dag,
+        )
 
-    task_3 = PostgresOperator(
-        task_id='load_poopoo_to_postgres',
-        postgres_conn_id="postgres_hockey",    
-        sql='SELECT * FROM time_airflow',
-        dag=ingestion_dag,
-    )
+        task_3 = PostgresOperator(
+            task_id='load_poopoo_to_postgres',
+            postgres_conn_id="postgres_hockey",    
+            sql='SELECT * FROM time_airflow',
+            dag=ingestion_dag,
+        )
 
-    task_4 = PostgresOperator(
-        task_id='load_poopoo_to_postgres_2',
-        postgres_conn_id="postgres_hockey",
-        sql='SELECT * FROM time_airflow WHERE id > %(num)s',
-        parameters={"num": 4000},
-        dag=ingestion_dag,
-    )
+        task_4 = PostgresOperator(
+            task_id='load_poopoo_to_postgres_2',
+            postgres_conn_id="postgres_hockey",
+            sql='SELECT * FROM time_airflow WHERE id > %(num)s',
+            parameters={"num": 4000},
+            dag=ingestion_dag,
+        )
+        task_1 >> task_2
+        # task_3 >> task_4
 
-with TaskGroup('Hockey ETL') as hockey_group:
-    task_5 = PythonOperator(
-        task_id='get_team_stats',
-        python_callable=get_team_stats,
-        dag=ingestion_dag,
-    )
+    with TaskGroup('Hockey_ETL') as hockey_group:
+        task_5 = PythonOperator(
+            task_id='get_team_stats',
+            python_callable=get_team_stats,
+            dag=ingestion_dag,
+        )
 
 
-task_1 >> task_2
-task_3 >> task_4
+# poopoo_group
+
+hockey_group
--- dags/utils/etl_utils.py
+++ dags/utils/etl_utils.py
@@ -4,27 +4,35 @@ import pandas as pd
 import random
 from sqlalchemy import create_engine
 
-PROCESSED_DATA_PATH = f'{os.getcwd()}/processed_data.csv'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PROCESSED_DATA_PATH = '/Users/danielcollins/airflow/processed_data.csv'
 
 def transform_poopoo(**kwargs):
     print('transform_poopoo')
+    print(kwargs)
     data = {}
     data['time'] = datetime.datetime.now()
     data['id'] = int(random.random()*10000)
     df = pd.DataFrame([data])
     print(df.shape)
     kwargs['ti'].xcom_push(key='df', value=df)
-    if (os.path.getsize(PROCESSED_DATA_PATH) == 0):
-        df.to_csv(PROCESSED_DATA_PATH, index=False)
-    else:
-        df.to_csv(PROCESSED_DATA_PATH, mode='a', header=False, index=False)
-    # df.to_csv(PROCESSED_DATA_PATH, index=False)
-
+    try:
+        if (os.path.getsize(PROCESSED_DATA_PATH) == 0):
+            df.to_csv(PROCESSED_DATA_PATH, index=False)
+        else:
+            df.to_csv(PROCESSED_DATA_PATH, mode='a', header=False, index=False)
+        # df.to_csv(PROCESSED_DATA_PATH, index=False)
+    except Exception as e:
+        print('error saving to csv')
+        print(e)
 
 def load_poopoo(**kwargs):
+    print(kwargs)
     passed_df = kwargs['ti'].xcom_pull(task_ids='transform_poopoo', key='df')
     df = pd.read_csv(PROCESSED_DATA_PATH)
-    print(f'passed: {passed_df.shape} read: {df.shape}')
+    # print(f'passed: {passed_df.shape} read: {df.shape}')
+    print(df.shape)
     # assert passed_df == df
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/hockey')
     try:
